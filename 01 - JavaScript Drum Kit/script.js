ab996e2e319eccdaa76b87b482dff952dae7576a
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-30 12:15:38 +0500

    Complete Day 1

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,21 @@
+function playSound(e) {
+  // audio is for playing sound
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  // key is for adding transition
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
